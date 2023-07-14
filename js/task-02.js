da441a12c2a11b0001e0c018cb7cb5f9aca66c23
Black Chaos <black.chaos.no1@gmail.com>
2023-07-14 16:47:40 +0300

    feat: add task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingridientsList = document.getElementById('ingredients');
+
+function createListItem(array) {
+  return array.map(el => {
+    const item = document.createElement('li');
+    const text = document.createElement('p');
+    item.className = 'item';
+    text.textContent = el;
+
+    item.prepend(text);
+
+    return item
+  })
+}
+
+ingridientsList.prepend(...createListItem(ingredients))
