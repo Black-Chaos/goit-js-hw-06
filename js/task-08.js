9c1599c263419f631644e545833053823e79d20a
Black Chaos <black.chaos.no1@gmail.com>
2023-07-17 14:00:24 +0300

    feat: add task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault();
+    const { elements: { email, password } } = this;
+    
+    if (!email.value || !password.value) alert('Please fill in all the fields!');
+
+    const formData = new FormData(this);
+    console.log(Object.fromEntries(formData));
+
+    this.reset()
+}
