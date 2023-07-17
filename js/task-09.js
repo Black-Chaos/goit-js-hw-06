0887741c9af797772e308041200ec0ea58e6c10b
Black Chaos <black.chaos.no1@gmail.com>
2023-07-17 14:16:22 +0300

    feat: add task #9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color')
+
+colorBtn.addEventListener('click', onChangeBodyColor)
+
+function onChangeBodyColor() {
+  const color = getRandomHexColor()
+  document.body.style.backgroundColor = color;
+  colorValue.textContent = color
+}
